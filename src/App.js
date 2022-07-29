a3a437eb66cc9b4ab9a40b02964a4a2b626ee82b
Panagiotis Gouvis <gouvispan@gmai.com>
2022-07-29 18:14:08 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Products from "./Pages/Products";
+import ErrorPage from "./Pages/ErrorPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/product" element={<Products/>}></Route>
+        <Route path="*" element={<ErrorPage/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Pages/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <strong> ERROR: PAGE NOT FOUND</strong>
+    </div>
+  );
+};
+
+export default ErrorPage;
