aeffb6afb151f5cbdc46179b82d0527d781926cc
Panagiotis Gouvis <gouvispan@gmai.com>
2022-08-16 14:02:43 +0300

    purseia last

--- src/Pages/Shop/Products/ProductList.js
+++ src/Pages/Shop/Products/ProductList.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Product from "./Product";
 import "./Products.css";
+import { useState } from "react";
 
 const ProductList = ({ products, addToCart }) => {
+  const [coosenCategory, setChoosenCategory] = useState(""); 
+  
+  const displayedProducts = myModels.filter(model => model.category === choosenModel.name);
+  console.log(displayedProducts);
+
   return (
     <div className="productList-container">
       {products.map((product) => (
