5c3977a455893b690ae0dd5b36ca7537f13e938d
Panagiotis Gouvis <gouvispan@gmai.com>
2022-08-29 07:23:18 +0300

    h

--- src/Pages/Shop/Products/ProductList.js
+++ src/Pages/Shop/Products/ProductList.js
@@ -3,28 +3,6 @@ import Product from "./Product";
 import "./Products.css";
 import { useSelector } from "react-redux";
 
-// const isInCategories = (categories, active) => {
-//   const flag = false;
-
-//   for (let i = 0; i < categories.length; i++) {
-//     if (categories[i].id === active.id) {
-//       flag = true;
-//     }
-//   }
-//   return flag;
-// };
-
-// const filteredArray = (arrayOfElements) =>
-//   arrayOfElements
-//     .filter((element) =>
-//       element.subElements.some((subElement) => subElement.surname === 1)
-//     )
-//     .map((element) => {
-//       let newElt = Object.assign({}, element); // copies element
-//       return newElt.subElements.filter(
-//         (subElement) => subElement.surname === "1"
-//       );
-//     });
 
 const ProductList = ({ addToCart }) => {
   const commerce = useSelector((state) => state.commerce);
