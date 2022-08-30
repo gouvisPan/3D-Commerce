40fb3f942582191814582fa8cb36575a6e771296
Panagiotis Gouvis <gouvispan@gmai.com>
2022-08-30 09:03:15 +0300

    wfe

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store/index";
+import {persistor, store} from "./store/index";
+import { PersistGate } from 'redux-persist/integration/react';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+   <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
   </Provider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,37 @@ import cartSlice from "./cart-slice";
 import uiSlice from "./ui-slice";
 import commerceSlice from "./commerce-slice";
 
-const store = configureStore({
-  reducer: {
-    cart: cartSlice.reducer,
-    ui: uiSlice.reducer,
-    commerce: commerceSlice.reducer
-  },
-});
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore,persistCombineReducers} from 'redux-persist';
+import combineReducers from 'redux'
+import thunk from 'redux-thunk';
 
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const userReducer = {
+  cart: cartSlice.reducer,
+  ui: uiSlice.reducer,
+  commerce: commerceSlice.reducer
+}
+const persistedReducer = persistCombineReducers(persistConfig, userReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk]
+})
+
+export const persistor = persistStore(store)
+
+// const store = configureStore({
+//   reducer: {
+//     cart: cartSlice.reducer,
+//     ui: uiSlice.reducer,
+//     commerce: commerceSlice.reducer
+//   },
+// });
+
+// export default store;
